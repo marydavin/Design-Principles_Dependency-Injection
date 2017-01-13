5c4880e5e32073d88cade8b615d5313989fceb7f
marydavin <mary.davin@cit.ie>
2017-01-13 11:38:45 +0000

    Add files via upload

---
+++ src/injector/EmailServiceInjector.java
@@ -0,0 +1,16 @@
+package injector;
+
+import consumer.Consumer;
+import consumer.MyDIApplication; 
+import service.EmailServiceImpl;
+
+public class EmailServiceInjector implements MessageServiceInjector {
+
+	@Override
+	public Consumer getConsumer() {
+		MyDIApplication app = new MyDIApplication();
+		app.setService(new EmailServiceImpl());
+		return app;
+	}
+
+}
